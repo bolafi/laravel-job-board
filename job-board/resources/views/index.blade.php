23a23e11c8583217f7e1f8000fd137be3a8a90a2
bolafi <>
2025-05-29 18:39:38 +0300

    Blade Templating Engine Module

---
+++ job-board/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <h2>My Home Page</h2>
+    <ul>
+        <li>Jobs Of Today</li>
+        <li>Jobs Trending</li>
+    </ul>
+</x-layout>
--- job-board/routes/web.php
+++ job-board/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\jobController;
+use App\Http\Controllers\indexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [indexController::class, 'index']);
+Route::get('/about', [indexController::class, 'about']);
+Route::get('/contact', [indexController::class, 'contact']);
 
 Route::get('/job', [jobController::class, 'index']);
